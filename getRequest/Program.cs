6d499fbc6f92a6062c90159e449ffa96d1b4921c
jorgeEF <jorgefemenia@gmail.com>
2023-03-16 18:34:04 -0300

    git repo created

---
+++ getRequest/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using Newtonsoft.Json;
+
+// Console.WriteLine("Hello, World!");
+string API_URL = "https://jsonplaceholder.typicode.com/posts?_limit=5";
+var client = new WebClient();
+var json = client.DownloadString(API_URL);
+//Console.WriteLine(json);
+dynamic posts = JsonConvert.DeserializeObject(json);
+
+foreach(var post in posts) {
+    Console.WriteLine(post.id + ": " + post.title);
+}
